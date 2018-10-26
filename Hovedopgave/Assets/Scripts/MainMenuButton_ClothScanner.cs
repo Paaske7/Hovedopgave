943e4cca93e77a84756441e21fc24b33d4b84c6b
Hovedopgave <paaske7@hotmail.com>
2018-10-26 15:55:55 +0200

    Main Menu
    Der er lavet en menu, hvor knappen åbner vuforia kameraet

---
+++ Hovedopgave/Assets/Scripts/MainMenuButton_ClothScanner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton_ClothScanner : MonoBehaviour {
+
+ 
+	public void changemenuscene()
+    {
+        SceneManager.LoadScene("ScanClothScene");
+    }
+}
