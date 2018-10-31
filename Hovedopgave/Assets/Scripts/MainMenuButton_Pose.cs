e7968b2e500747d4433a87ea672181c7c6d8b50a
Hovedopgave <paaske7@hotmail.com>
2018-10-31 21:36:29 +0100

    asd
    asd

---
+++ Hovedopgave/Assets/Scripts/MainMenuButton_Pose.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton_Pose : MonoBehaviour {
+
+    public void changemenuscene()
+    {
+        SceneManager.LoadScene("PoseScene");
+    }
+}
