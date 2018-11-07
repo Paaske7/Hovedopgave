7e561771ad44904ca4e4c006d970013f88ecca8a
Hovedopgave <paaske7@hotmail.com>
2018-11-07 11:48:40 +0100

    Database working
    YES

--- Hovedopgave/Assets/Scripts/Database.cs
+++ Hovedopgave/Assets/Scripts/Database.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 
 public class Database : MonoBehaviour
 {
-    private readonly string connectionString = "Data Source=.;Initial Catalog = Clothing; Integrated Security = True" ;
+
+    private string connectionString = @"Data Source = tcp:hovedopgave.database.windows.net,1433; 
+     user id = Mikael;
+     password = Paaske77;
+     Initial Catalog = Clothing;";
+        
+//        @"Data Source=tcp:hovedopgave.database.windows.net,1433;Initial Catalog=Clothing;
+//Persist Security Info=False;User ID=Mikael;Password=Paaske77;MultipleActiveResultSets=False;Encrypt=True;
+//TrustServerCertificate=False;Connection Timeout=30;";
+                              
     SqlConnection connection = null;
 
 
@@ -22,8 +31,10 @@ public class Database : MonoBehaviour
         List<Cloth> ClothList = new List<Cloth>();
         DataTable dataTable = new DataTable();
 
-            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Cloths Series where Name = 'T-Shirt'", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Cloths where Name = 'T-Shirt'", connection);
+        Debug.Log("Prøver at oprette forbindelse");
             connection.Open();
+        Debug.Log("Forbindelse oprettet");
             adapter.Fill(dataTable);
             connection.Close();
 
