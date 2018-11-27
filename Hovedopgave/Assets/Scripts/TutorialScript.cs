a9000d40ba545f86e5d86468b7920488ecd5b88c
Hovedopgave <paaske7@hotmail.com>
2018-11-27 14:14:51 +0100

    FirstTimeOpening game check
    .

--- Hovedopgave/Assets/Scripts/TutorialScript.cs
+++ Hovedopgave/Assets/Scripts/TutorialScript.cs
@@ -6,6 +6,28 @@ using UnityEngine.UI;
 public class TutorialScript : MonoBehaviour
 {
     // Mikael Paaske
+    public GameObject toturialText1;
+
+    void Start()
+    {
+        // Checker om det er første gang appen kører.
+        toturialText1 = GameObject.Find("TutorialText1");
+        if (PlayerPrefs.GetInt("FIRSTTIMEOPENING", 1) == 1)
+        {
+            toturialText1.SetActive(true);
+            Debug.Log("First Time Opening");
+
+            //Set first time opening to false
+            PlayerPrefs.SetInt("FIRSTTIMEOPENING", 0);
+
+        }
+        else
+        {
+            Debug.Log("NOT First Time Opening");
+            toturialText1.SetActive(false);
+            //Do your stuff here
+        }
+    }
     // Fjerner kun GameObjekter der har en Box Collider og da det kun er vores Toturial tekster der har dette er et tjek efter navn ikke nødvendig.
     void Update()
     {
