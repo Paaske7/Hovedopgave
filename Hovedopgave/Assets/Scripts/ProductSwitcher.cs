5ce3ea1308aab54f46d02c829f6bb209576d3014
Hovedopgave <paaske7@hotmail.com>
2018-11-28 17:10:08 +0100

    SexChange check settings
    .

--- Hovedopgave/Assets/Scripts/ProductSwitcher.cs
+++ Hovedopgave/Assets/Scripts/ProductSwitcher.cs
@@ -5,40 +5,58 @@ using UnityEngine;
 
 public class ProductSwitcher : MonoBehaviour
 {
+    // Mikael Paaske
+    // Laver en ny bool og sætter den = bool fra SettingsScript
+    bool getSexchangeFromSettings = SettingsScript.Sexchange;
 
-    // Use this for initialization
-    private bool defaultModel;
+
+    // Thomas Nielsen
+    public bool defaultModel = true;
     public GameObject modelPlaceHolder;
     private string currentModelName;
-
+    SettingsScript settingsScript;
     private string itemSelected;
     public GameObject productPlaceHolder;
     private GameObject parentModel;
-
     public GameObject SelectedButton;
-
     //Load alle nye modeller ind i projektet under 'Models>Products>data/sæson
     //Alle tilgængelige produkter findes fra databasen og vises i en menu så brugeren kan vælge
     //Når brugeren vælger et produkt bruges info fra DB til at finde modelNavnet
     //Modellen bliver vist i Unity
     //brugeren kan vælge et nyt produkt hvorefter den forrige bliver fjernet (Man kunne lave noget kategorisering så man både kan vise bukser og trøjer samtidig)
-
     //lav seperat kode til visning og valg af produkter
     //grid med billed og tekst der virker som en knap til at vælge produkt med
     //laves automatisk baseret på de tilgængelige produkter
-
-
     //Mobil controls er lidt bæ
     //Kan ikke kigge nedefra
     //lav knap til reset af kamera?
     //lav simple pile til rotering ?
     //sæt en fast scale for prefabs så en par jeans ikke er 8 meter høj
+
+
     private void Start()
     {
-        defaultModel = true;
+        // Mikael Paaske
+        // checkter om knappen fra Settings er trykket på - Det vil sige om der er trykket skift køn 
         parentModel = GameObject.Find("Product Models").GetComponent<GameObject>();
+        if (getSexchangeFromSettings == false)
+        {
+            modelPlaceHolder = Resources.Load<GameObject>("Prefabs/xbot");
+            Instantiate(modelPlaceHolder, Vector3.zero, Quaternion.Euler(0, 180, 0));
+        }
+        else if (getSexchangeFromSettings == true)
+        {
+            currentModelName = "ybot";
+            modelPlaceHolder = Resources.Load<GameObject>("Prefabs/ybot");
+            Instantiate(modelPlaceHolder, Vector3.zero, Quaternion.Euler(0, 180, 0));
+        }
+        else
+        {
+            modelPlaceHolder = Resources.Load<GameObject>("Prefabs/xbot");
+            Instantiate(modelPlaceHolder, Vector3.zero, Quaternion.Euler(0, 180, 0));
+        }
     }
-
+    // Thomas Nielsen
     public void ChangeSex()
     {
         Debug.Log(defaultModel);
---
+++ Hovedopgave/Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsScript : MonoBehaviour {
+
+    private ProductSwitcher productSwitcher;
+    public static bool Sexchange = true;
+    // Use this for initialization
+    void Update () {
+        //if (Sexchange == true)
+        //{
+        //    productSwitcher.defaultModel = true;
+        //}
+        //else if (Sexchange == false)
+        //{
+        //    productSwitcher.defaultModel = false;
+        //}
+        //else
+        //{
+        //    Debug.Log("Fejl");
+        //}
+    }
+	
+	public void SettingsChangeSexMale()
+    {
+        Sexchange = true;
+        Debug.Log("Male");
+    }
+    public void SettingsChangeSexFemale()
+    {
+        Sexchange = false;
+        Debug.Log("Female");
+    }
+}
