4e87ce9371939fc2296af0a4ac196fc10f4544d6
Thoms-DM <mini_hippo@hotmail.com>
2018-11-28 12:26:49 +0100

    Added copypaste button

---
+++ Hovedopgave/Assets/Scripts/copypaste.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copypaste : MonoBehaviour {
+
+    public void CopyPaste()
+    {
+        UniClipboard.SetText("HEST DEMOTEKST www.hestedemo.dk");
+    }
+}
