69ef149d6717190dd13322b8635949a1ceb399f6
Hovedopgave <paaske7@hotmail.com>
2018-12-11 14:20:49 +0100

    Mikael: Done med design
    .

--- Hovedopgave/Assets/Scripts/SettingsScript.cs
+++ Hovedopgave/Assets/Scripts/SettingsScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour {
 
@@ -6,29 +7,64 @@ public class SettingsScript : MonoBehaviour {
     public static bool isMale;
     public static bool showTutorial;
     public static bool openInApp;
-	
-	public void ChangeSexToMale()
+
+    public GameObject canvas;
+    public GameObject showSexText;
+    public GameObject ønskeskyenText;
+    public GameObject tutorialText;
+
+    public void Start()
     {
-        // Hvis tryk på knappen "Mand" i indstillinger menuen bliver boolen sat til true
-        isMale = true;
+        canvas = GameObject.Find("SettingsCanvas");
+        showSexText = canvas.transform.Find("ShowSexText").gameObject;
+        ønskeskyenText = canvas.transform.Find("ShowØnskeskyenText").gameObject;
+        tutorialText = canvas.transform.Find("ShowToturialText").gameObject;
+
     }
-    public void ChangeSexToFemale()
+	public void ChangeSex()
     {
-        // Hvis tryk på knappen "Kvinde" i indstillinger menuen bliver boolen sat til false
-        isMale = false;
+        // Hvis slider bliver rykket til den ene ende, bliver boolen sat til det modsatte af før
+        isMale = !isMale;
+
+        // Ændrer gameobjektets tekst ud fra boolen er sand eller falsk
+        if (isMale == true)
+        {
+            showSexText.GetComponent<Text>().text = "Mand";
+        }
+        else if (isMale == false)
+        {
+            showSexText.GetComponent<Text>().text = "Kvinde";
+        }
     }
 
     public void ShowToturial()
     {
-        // Sætter boolen til True, så tutorial kan blive vist igen
-        showTutorial = true;
+        // Hvis slider bliver rykket til den ene ende, bliver boolen sat til det modsatte af før
+        showTutorial = !showTutorial;
+
+        // Ændrer gameobjektets tekst ud fra boolen er sand eller falsk
+        if (showTutorial == true)
+        {
+            tutorialText.GetComponent<Text>().text = "Vis";
+        }
+        else if (showTutorial == false)
+        {
+            tutorialText.GetComponent<Text>().text = "Vis ikke";
+        }  
     }
-    public void WishListOpenInApp()
+    public void WishList()
     {
-        openInApp = true;
-    }
-    public void WishListOpenInWebsite()
-    {
-        openInApp = false;
+        // Hvis slider bliver rykket til den ene ende, bliver boolen sat til det modsatte af før
+        openInApp = !openInApp;
+
+        // Ændrer gameobjektets tekst ud fra boolen er sand eller falsk
+        if (openInApp == true)
+        {
+            ønskeskyenText.GetComponent<Text>().text = "App";
+        }
+        else if (openInApp == false)
+        {
+            ønskeskyenText.GetComponent<Text>().text = "Hjemmeside";
+        }
     }
 }
